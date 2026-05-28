5557fe10341c8a873be9bb0e1a0a14917a8ed47a
survivor3550-source <survivor3550@gmail.com>
2026-05-28 10:44:43 +0000

    remove vite cache files

--- .vite/deps/react-dom.js
+++
@@ -1,7 +0,0 @@
-import {
-  require_react_dom
-} from "./chunk-OS4664QW.js";
-import "./chunk-4SFRHSJ3.js";
-import "./chunk-EQCVQC35.js";
-export default require_react_dom();
-//# sourceMappingURL=react-dom.js.map
